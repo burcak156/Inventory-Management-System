3e068e39c20ff893007f5b6a36bdfa5b7ad96e41
stjbytd <stjbytd@stjbytd.tcmb.gov.tr>
2016-08-17 17:04:51 +0300

    ...

--- src/main/java/com/sprhib/controller/ApplicationController.java
+++ src/main/java/com/sprhib/controller/ApplicationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -33,6 +34,16 @@ public class ApplicationController {
 		List<Application> applications = applicationService.getApplications();
 		modelAndView.addObject("applications", applications);
 		
+		return modelAndView;
+	}
+	
+
+	@RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
+	public ModelAndView deleteApplication(@PathVariable Integer id) {
+		ModelAndView modelAndView = new ModelAndView("home");
+		applicationService.deleteApplication(id);
+		String message = "Application was succefuly deleted.";
+		modelAndView.addObject("message", message);
 		return modelAndView;
 	}
 }
